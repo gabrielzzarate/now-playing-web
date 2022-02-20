c297d6ff6e17cca42f2510d9778c234bd76c5bdf
Gabriel Zarate <gabrielzzarate@gmail.com>
2022-02-20 08:01:02 -0500

    add user collection in deb

---
+++ src/lib/mongo/index.ts
@@ -0,0 +1,55 @@
+import { Db, MongoClient, MongoClientOptions } from 'mongodb'
+
+const MONGODB_URI = process.env.MONGODB_URI ?? ''
+const MONGO_DB = process.env.MONGO_DB_NAME ?? ''
+const options = {}
+
+let cached = global.mongo
+let client
+let clientPromise: Promise<MongoClient>
+
+if (!cached) {
+  cached = global.mongo = { conn: null, promise: null }
+}
+
+if (process.env.NODE_ENV === 'development') {
+  // In development mode, use a global variable so that the value
+  // is preserved across module reloads caused by HMR (Hot Module Replacement).
+  if (!global._mongoClientPromise) {
+    client = new MongoClient(MONGODB_URI, options)
+    global._mongoClientPromise = client.connect()
+  }
+  clientPromise = global._mongoClientPromise
+} else {
+  // In production mode, it's best to not use a global variable.
+  client = new MongoClient(MONGODB_URI, options)
+  clientPromise = client.connect()
+}
+
+const connectToDatabase = async (): Promise<{ client: MongoClient; db: Db }> => {
+  if (cached.conn) {
+    return cached.conn
+  }
+
+  if (!cached.promise) {
+    const options: MongoClientOptions = {}
+
+    cached.promise = MongoClient.connect(MONGODB_URI, options).then((client) => {
+      return {
+        client,
+        db: client.db(MONGO_DB)
+      }
+    })
+  }
+
+  cached.conn = await cached.promise
+
+  return cached.conn
+}
+
+export async function withMongo<T>(fn: (db: Db) => Promise<T>): Promise<T> {
+  const conn = await connectToDatabase()
+  return await fn(conn.db)
+}
+
+export default clientPromise
---
+++ src/pages/api/user.ts
@@ -0,0 +1,43 @@
+import { withMongo } from '@lib/mongo'
+import { Db } from 'mongodb'
+import { nanoid } from 'nanoid'
+import { NextApiRequest, NextApiResponse } from 'next'
+import nc from 'next-connect'
+
+interface Base {
+  id: string
+  created: string
+  updated: string
+}
+
+interface User extends Base {
+  email: string
+}
+
+export default nc<NextApiRequest, NextApiResponse>()
+  .get(async (req, res) => {
+    const users = await withMongo(async (db: Db) => {
+      const collection = db.collection<User>('users')
+
+      return await collection.find().toArray()
+    })
+
+    return res.json(users)
+  })
+  .post(async (req, res) => {
+    // if (!isValidEmail(req.body.email)) {
+    //   return res.status(400).json({ error: 'invalid-email' })
+    // }
+
+    await withMongo(async (db: Db) => {
+      const collection = db.collection<User>('users')
+      await collection.insertOne({
+        id: nanoid(),
+        email: req.body.email,
+        created: new Date().toISOString(),
+        updated: new Date().toISOString()
+      })
+
+      return res.status(204).end()
+    })
+  })
