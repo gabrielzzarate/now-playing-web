61acc4c75fade29b1feffdd1ddc7f368c2f31b57
Gabriel Zarate <gabrielzzarate@gmail.com>
2022-02-20 08:15:31 -0500

    cleanup types for mongo connection

--- src/lib/mongo/index.ts
+++ src/lib/mongo/index.ts
@@ -1,29 +1,38 @@
 import { Db, MongoClient, MongoClientOptions } from 'mongodb'
 
+// for reference: https://stackoverflow.com/questions/65779464/cache-mongodb-connection-with-next-js-10-typescript-project-api-route
+
 const MONGODB_URI = process.env.MONGODB_URI ?? ''
-const MONGO_DB = process.env.MONGO_DB_NAME ?? ''
+const MONGO_DB_NAME = process.env.MONGO_DB_NAME ?? ''
 const options = {}
 
-// @ts-expect-error
+declare global {
+  namespace NodeJS {
+    interface Global {
+      mongo: {
+        conn?: MongoClient | null
+        promise?: Promise<MongoClient> | null
+      }
+      _mongoClientPromise: Promise<MongoClient> | null
+    }
+  }
+}
+
 let cached = global.mongo
 let client
 let clientPromise: Promise<MongoClient>
 
 if (!cached) {
-  // @ts-expect-error
   cached = global.mongo = { conn: null, promise: null }
 }
 
 if (process.env.NODE_ENV === 'development') {
   // In development mode, use a global variable so that the value
   // is preserved across module reloads caused by HMR (Hot Module Replacement).
-  // @ts-expect-error
   if (!global._mongoClientPromise) {
     client = new MongoClient(MONGODB_URI, options)
-    // @ts-expect-error
     global._mongoClientPromise = client.connect()
   }
-  // @ts-expect-error
   clientPromise = global._mongoClientPromise
 } else {
   // In production mode, it's best to not use a global variable.
@@ -33,22 +42,25 @@ if (process.env.NODE_ENV === 'development') {
 
 const connectToDatabase = async (): Promise<{ client: MongoClient; db: Db }> => {
   if (cached.conn) {
+    // @ts-expect-error
     return cached.conn
   }
 
   if (!cached.promise) {
     const options: MongoClientOptions = {}
 
+    // @ts-expect-error
     cached.promise = MongoClient.connect(MONGODB_URI, options).then((client) => {
       return {
         client,
-        db: client.db(MONGO_DB)
+        db: client.db(MONGO_DB_NAME)
       }
     })
   }
 
   cached.conn = await cached.promise
 
+  // @ts-expect-error
   return cached.conn
 }
 
